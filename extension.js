d161f3ab2267cbb4601580afa9e255787248ac12
Kushagra Karira <kushagrakarira@gmail.com>
2021-02-25 19:16:39 +0530

    Febuary update

---
+++ extension.js
@@ -0,0 +1,3 @@
+function init () {} // will be called first to initiate the extenion
+function enable () {} // will be called when it is enabled
+function disable() {} // will be called when it is disabled
