a673632cb86cc78505386e98bb75ac810e82efb9
Kushagra Karira <kushagrakarira@gmail.com>
2021-02-26 13:21:55 +0530

    Febuary update

--- extension.js
+++ extension.js
@@ -1,3 +1,3 @@
-function init () {} // will be called first to initiate the extenion
-function enable () {} // will be called when it is enabled
-function disable() {} // will be called when it is disabled
+function init () {} // will be called first to initiate the extenion //pull the image locally
+function enable () {gsettings set org.gnome.desktop.background picture-uri "https://source.unsplash.com/daily"} // will be called when it is enabled
+function disable() {} // will be called when it is disabled //reset to default wallpaper
