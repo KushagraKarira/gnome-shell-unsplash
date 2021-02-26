8b761cd5d898742482e26469fa33545f7addee10
Kushagra Karira <kushagrakarira@gmail.com>
2021-02-26 16:33:25 +0530

    Febuary update

--- extension.js
+++ extension.js
@@ -1,3 +1,6 @@
-function init () {} // will be called first to initiate the extenion //pull the image locally
-function enable () {gsettings set org.gnome.desktop.background picture-uri "https://source.unsplash.com/daily"} // will be called when it is enabled
+function init () {}
+function enable () {
+cd ~/Pictures/Wallpapers/ && rm daily && wget https://source.unsplash.com/user/nasa/daily && gsettings set org.gnome.desktop.background picture-uri ~/Pictures/Wallpapers/daily
+} // Goes to wallpaper directory, deletes old image, pulls new daily one from unsplash and makes it the wallpaper, voila !
+
 function disable() {} // will be called when it is disabled //reset to default wallpaper
